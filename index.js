00ac4612d785832a60d2413ae88f6f58218d3d6b
carlosortegon <carlosortegon@pulsar.la>
2019-12-10 23:14:10 -0500

    dbc funcionand

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ require('chromedriver');
 var path = require('chromedriver').path;
 var fs = require('fs');
 //Call Cpatcha
-dbc = require("deathbycaptcha/deathbycaptcha");
-const client = new dbc.SocketClient(username, password);
+dbc = require("./deathbycaptcha/deathbycaptcha.js");
+const client = new dbc.SocketClient("ortegon.carlos@gmail.com", "nm80vdBUE4OX26gB2UyL");
+
+client.get_balance((balance) => {console.log(balance)})
+
 
 
 function solvecaptcha(imagen) {
-  var fileimage = fs.readFileSync(imagen)
-  console.log('iniciando solvecpathca',fileimage)
-  dbc.solve(fileimage, function(err, id, solution) {
-    console.log('dbc')
-    if(err) return console.error(err); // onoes!
-      console.log(solution)
-      return solution; 
-    });
+  console.log('iniciando solvecpathca')
+  client.upload({captcha:imagen,extra: {}}, (captcha) => {console.log(captcha)})
 };
 
 
+
 solvecaptcha('./captcha.jpg')
+
+
+// client.get_captcha(1407421268, (captcha) => {console.log(captcha)})
 
 // (async function example() {
 //   let driver = await new Builder().forBrowser('chrome').build();
@@ -34,6 +35,8 @@ solvecaptcha('./captcha.jpg')
 //     	}
 //     )
 //     driver.wait(until.elementLocated(By.name('cedulaInput')), 10000).sendKeys("80076057");
+//     // var captcha_obj = solvecaptcha('./captcha.jpg')
+
 
     	
 //   } finally {
